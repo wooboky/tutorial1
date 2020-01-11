0538fe43491599e0b3cb89e435ac3b5905ce6f9c
wooboky <wooboky@gmail.com>
2020-01-11 23:02:18 +0900

    edit A1, A2

--- src/com/midas/tu/A1.java
+++ src/com/midas/tu/A1.java
@@ -3,5 +3,7 @@ package com.midas.tu;
 public class A1 {
 	public static void main(String[] args) {
 		System.out.println("main method of This Project.");
+		A2 a2 = new A2();
+		a2.getSummary();
 	}
 }
--- src/com/midas/tu/A2.java
+++ src/com/midas/tu/A2.java
@@ -4,6 +4,7 @@ public class A2 {
 	public void getSummary() {
 		System.out.println("A2");
 		System.out.println("A2");
+		System.out.println("A2");
 	}
 	
 }
